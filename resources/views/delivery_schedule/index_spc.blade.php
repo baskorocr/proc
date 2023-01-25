1552c54235b4bec7dffcd4f299e871557d23120d
Irfa Ardiansyah <irfardy2@gmail.com>
2023-01-25 10:08:59 +0700

    FIx Typo

--- resources/views/delivery_schedule/index_spc.blade.php
+++ resources/views/delivery_schedule/index_spc.blade.php
@@ -7,7 +7,7 @@
 		<nav>
 			<ol class="breadcrumb">
 				<li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
-				<li class="breadcrumb-item active"><a href="{{route('delivery.schedule.spc')}}">Download Schedule Spc</a></li>
+				<li class="breadcrumb-item active"><a href="{{route('delivery.schedule.spc')}}">Download  Spc Order</a></li>
 			</ol>
 		</nav>
 		</div><!-- End Page Title -->
