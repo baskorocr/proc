92cc415c2a8b4b860d61f04b61d6456438438201
Irfa Ardiansyah <irfardy2@gmail.com>
2023-02-16 11:21:53 +0700

    delivery

--- resources/views/project_management/file_master/index.blade.php
+++ resources/views/project_management/file_master/index.blade.php
@@ -16,8 +16,8 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            
-                            <table id="example2" class="table table-bordered table-striped">
+                            <div class="table-responsive mt-3 ">
+                            <table id="filemaster" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
                                         <th>No</th>
@@ -27,23 +27,31 @@
                                 </thead>
                                 <tbody>
                                     <?php
+
                                     $no = 1;
-                                    $dir_iterator = new RecursiveDirectoryIterator(public_path('DATA/'));
-                                    $iterator = new RecursiveTreeIterator($dir_iterator);
-                                    foreach ($iterator as $file) {
-                                    $string = explode(".", $file);
+                                    // $dir_iterator = new RecursiveDirectoryIterator(public_path('DATA/'));
+                                    // $iterator = new RecursiveTreeIterator($dir_iterator);
+                                    $iterator = File::allFiles(public_path('DATA/'));
+                                    // dd($iterator);
+                                    foreach ($iterator as $path) {
+                                        // dd();
+                                    $file = pathinfo($path);
+                                    $fullPath = $path->getRelativePathname();
+                                    $string = explode(".", $fullPath);
+
+                                    // dd($file);
                                     if(count($string) > 1) {
                                     $data = $string[1];
                                     }
-                                    $myfile = str_replace('DATA\\',"",$file);
-                                    $myfile = explode('\\DATA\\',$file);
-                                    if ($data == "pdf" and strpos($file, '.pdf') !== false) {
+                                    $myfile = str_replace('DATA\\',"",$fullPath);
+                                    $myfile = explode('\\DATA\\',$fullPath);
+                                    if ($data == "pdf" and strpos($fullPath, '.pdf') !== false) {
                                     echo "
                                     <tr>
                                         <td>$no</td>
-                                        <td>$file</td>
+                                        <td>".$file['basename']."</td>
                                         <td>
-                                            <a href='".asset('DATA')."/view_data.php?p=$myfile[1]' target='_blank' data-toggle='tooltip' title='click to download'>
+                                            <a href='".asset('DATA')."/view_data.php?p=$myfile[0]' target='_blank' data-toggle='tooltip' title='click to download'>
                                                
                                                 <span class='badge bg-success'>Download <i class='fa fa-download'></i></span>
                                                 
@@ -58,6 +66,7 @@
                                 </tbody>
                             </table>
                         </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -66,5 +75,7 @@
 
     @endsection
     @section('javascript')
-    
+    <script type="text/javascript">
+        $('#filemaster').DataTable({})
+    </script>
     @endsection
