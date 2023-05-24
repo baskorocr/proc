244bcb518e5d054c642c679ea779db1bcce52b02
abdul haris <haris@tech.kelola.biz>
2023-05-25 06:33:07 +0700

    update message status api SAP to eproc

--- app/Http/Controllers/ManifestController.php
+++ app/Http/Controllers/ManifestController.php
@@ -88,44 +88,57 @@ class ManifestController extends Controller
 
     public function sendManifest(Request $request)
     {
+        // $cekManifest = ManifestHeader::where('manifest', $request->manifest)->first();
         $vendor = Vendor::where('id_vendor', $request->id_vendor)->first();
         $msg_mail = '';
+        $msg_data = '';
+
+        $cekManifest = ManifestHeader::firstOrNew([
+            'manifest' => $request->manifest
+        ]);
+
+        if($cekManifest || $cekManifest->exists()) {
+            $msg_data = 'Manifest send to eproc failed, cannot insert duplicate data manifest.';
+
+        } else {
+            $manifest = new ManifestHeader;
+            $manifest->manifest = $request->manifest;
+            $manifest->id_vendor = $request->id_vendor;
+            $manifest->delivery_time = $request->delivery_time;
+            $manifest->delivery_date = $request->delivery_date;
+            $manifest->po_num = $request->po_num;
+            $manifest->file_nm = $request->file_nm;
+            $manifest->mf_type = $request->mf_type;
+            $manifest->release_date = $request->release_date;
+            $manifest->save();
+            
+            foreach ($request->details as $detail){
+                $manifest_detail = new ManifestDetail;
+                $manifest_detail->kanban = $detail['kanban'];
+                $manifest_detail->seq_kanban = $detail['seq_kanban'];
+                $manifest_detail->item = $detail['item'];
+                $manifest_detail->manifest = $request->manifest;
+                $manifest_detail->material = $detail['material'];
+                $manifest_detail->material_desc = $detail['material_desc'];
+                $manifest_detail->qty_pack = $detail['qty_pack'];
+                $manifest_detail->qty_in = $detail['qty_in'];
+                $manifest_detail->arrival_date = Carbon::parse($detail['arrival_date']);
+                $manifest_detail->arrival_time = Carbon::parse($detail['arrival_time']);
+                $manifest_detail->scan_date = Carbon::parse($detail['scan_date']);
+                $manifest_detail->scan_time = Carbon::parse($detail['scan_time']);
+                $manifest_detail->scan_by = $detail['scan_by'];
+                $manifest_detail->issued_date = Carbon::parse($detail['issued_date']);
+                $manifest_detail->issued_time = Carbon::parse($detail['issued_time']);
+                $manifest_detail->issued_by = $detail['issued_by'];
+                $manifest_detail->active = $detail['active'];
+                $manifest_detail->save();
+            }
+
+            $msg_data = 'Manifest send to eproc saved successfully.';
+        }
 
         if($vendor) {
             if($vendor->status_vendor === 'A') {
-                $manifest = new ManifestHeader;
-                $manifest->manifest = $request->manifest;
-                $manifest->id_vendor = $request->id_vendor;
-                $manifest->delivery_time = $request->delivery_time;
-                $manifest->delivery_date = $request->delivery_date;
-                $manifest->po_num = $request->po_num;
-                $manifest->file_nm = $request->file_nm;
-                $manifest->mf_type = $request->mf_type;
-                $manifest->release_date = $request->release_date;
-                $manifest->save();
-        
-                foreach ($request->details as $detail){
-                    $manifest_detail = new ManifestDetail;
-                    $manifest_detail->kanban = $detail['kanban'];
-                    $manifest_detail->seq_kanban = $detail['seq_kanban'];
-                    $manifest_detail->item = $detail['item'];
-                    $manifest_detail->manifest = $request->manifest;
-                    $manifest_detail->material = $detail['material'];
-                    $manifest_detail->material_desc = $detail['material_desc'];
-                    $manifest_detail->qty_pack = $detail['qty_pack'];
-                    $manifest_detail->qty_in = $detail['qty_in'];
-                    $manifest_detail->arrival_date = Carbon::parse($detail['arrival_date']);
-                    $manifest_detail->arrival_time = Carbon::parse($detail['arrival_time']);
-                    $manifest_detail->scan_date = Carbon::parse($detail['scan_date']);
-                    $manifest_detail->scan_time = Carbon::parse($detail['scan_time']);
-                    $manifest_detail->scan_by = $detail['scan_by'];
-                    $manifest_detail->issued_date = Carbon::parse($detail['issued_date']);
-                    $manifest_detail->issued_time = Carbon::parse($detail['issued_time']);
-                    $manifest_detail->issued_by = $detail['issued_by'];
-                    $manifest_detail->active = $detail['active'];
-                    $manifest_detail->save();
-                }
-
                 try {
                     $this->mailer_send($request->manifest);
                     $msg_mail = 'Manifest send mailed to vendor successfully.';
@@ -134,30 +147,23 @@ class ManifestController extends Controller
                 }
 
                 return response()->json([
-                    'msg_data' => 'Manifest send to eproc saved successfully',
+                    'msg_data' => $msg_data,
                     'msg_mail' => $msg_mail,
                     'data' => $manifest,
                 ], 200);
 
             } else {
                 return response()->json([
-                    'msg_data' => 'Manifest send to eproc failed, Status vendor not active, please confirm to administrator!',
+                    'msg_data' => $msg_data,
                     'msg_mail' => 'Manifest send to vendor failed.',
-                ], 422);
+                ], 200);
             }
         } else {
             return response()->json([
-                'msg_data' => 'Manifest send to eproc failed, Vendor Not Found!',
+                'msg_data' => $msg_data,
                 'msg_mail' => 'Manifest send to vendor failed.',
             ], 422);
         }
-
-
-        
-        // $this->mailer_send($request->manifest);
-        // Mail::to($vendor->vend_email)->send(new ManifestMail($manifest, $vendor));
-
-        
         
     }
 
@@ -167,6 +173,7 @@ class ManifestController extends Controller
         $mannifest = ManifestHeader::where('manifest', $request->manifest)->first();
         $vendor = Vendor::where('id_vendor', $request->id_vendor)->first();
         $msg_mail = '';
+        $msg_data = 'Manifest send to eproc saved successfully.';
 
         if($vendor){
             if($vendor->status_vendor === 'A'){
@@ -175,27 +182,27 @@ class ManifestController extends Controller
                 try {
                     $this->mailer_send($mannifest);
                     // Mail::to($user->username)->send(new PoMail($po, $user));
-                    $msg_mail = 'PO send mailed to vendor successfully';
+                    $msg_mail = 'Manifest send mailed to vendor successfully';
                 } catch (Exception $e) {
                     $msg_mail = $e->getMessage();
                 }
                 
 
                 return response()->json([
-                    'msg_data' => 'Manifest send to eproc saved successfully.',
+                    'msg_data' => $msg_data,
                     'msg_mail' => $msg_mail,
-                    'data' => $po
+                    'data' => $manifest
                 ], 200);
             } else {
                 return response()->json([
-                    'msg_data' => 'Manifest send to eproc failed, Mail user not active!',
+                    'msg_data' => $msg_data,
                     'msg_mail' => 'Manifest send to vendor failed.',
                     //'data' => $po
                 ], 422);
             }
         } else {
             return response()->json([
-                'msg_data' => 'Manifest send to eproc failed, ID User Not Found!',
+                'msg_data' => $msg_data,
                 'msg_mail' => 'Manifest send to vendor failed.',
                 //'data' => $po
             ], 422);
