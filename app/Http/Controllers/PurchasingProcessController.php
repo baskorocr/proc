7e0f13bf835bd26af30cd67f2637df093d317383
Irfa Ardiansyah <irfardy2@gmail.com>
2023-05-31 10:41:33 +0700

    Import PO

--- app/Http/Controllers/PurchasingProcessController.php
+++ app/Http/Controllers/PurchasingProcessController.php
@@ -439,7 +439,7 @@ class PurchasingProcessController extends Controller
         ->editColumn('total_amount', function($data){
             
 
-            $total_value = number_format($data->tot_va, 2, '.', ',');
+            $total_value = number_format($data->tot_val, 2, '.', ',');
             $tot_val   = "<p class='text-right' >".$total_value."</p>";
 
             return @$tot_val ;
@@ -684,7 +684,7 @@ class PurchasingProcessController extends Controller
         ->editColumn('total_amount', function($data){
             
 
-            $total_value = number_format($data->tot_va, 2, '.', ',');
+            $total_value = number_format($data->tot_val, 2, '.', ',');
             $tot_val   = "<p class='text-right' >".$total_value."</p>";
 
             return @$tot_val ;
