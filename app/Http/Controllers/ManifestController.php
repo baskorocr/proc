669b058a4bef8d038f081811b703949b8b85e325
abdul haris <haris@tech.kelola.biz>
2023-06-08 16:09:12 +0700

    update scan to sap

--- app/Http/Controllers/ManifestController.php
+++ app/Http/Controllers/ManifestController.php
@@ -92,7 +92,7 @@ class ManifestController extends Controller
                         $vendor = Vendor::where('id_vendor', $manifest->id_vendor)->first();
                         // change all! the old code is all wrong
                         $getManifestDetail = ManifestDetail::where('manifest', $manifest->manifest)->get();
-                        $groupByMaterial = $getManifestDetail->groupBy('manterial');
+                        $groupByMaterial = $getManifestDetail->groupBy('material');
                         $mapMaterial = $groupByMaterial->map(function($data){
                             return [
                                 'material' => $data->first()->material,
